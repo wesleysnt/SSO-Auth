a1188641d346404630227b3a1275c130c398dae7
Wesley Santana <wesley.santana@redtech.co.id>
2024-06-27 14:01:53 +0700

    .

--- cmd/cli/commands/root.go
+++ cmd/cli/commands/root.go
@@ -1,6 +1,7 @@
 package commands
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/spf13/cobra"
@@ -8,17 +9,9 @@ import (
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:   "sso",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Run: func(command *cobra.Command, args []string) {
+		fmt.Print("")
+	},
 }
 
 // Use: "wekekeke",
@@ -51,5 +44,5 @@ func init() {
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
-	cobra.OnInitialize()
+	// rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
