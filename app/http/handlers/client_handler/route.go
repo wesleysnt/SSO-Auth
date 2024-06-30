32e8b02988a1a1d918d83e6dd9ec23246b943d2a
Wesley Santana <wesley.santana@redtech.co.id>
2024-06-30 10:14:51 +0700

    add

--- app/http/handlers/client_handler/route.go
+++ app/http/handlers/client_handler/route.go
@@ -12,4 +12,9 @@ func ClientRoute(route fiber.Router) {
 	auth := route.Group("/client")
 
 	auth.Post("/", middleware.NewAuthMMiddleware(), handler.Create)
+	auth.Get("/", middleware.NewAuthMMiddleware(), handler.List)
+	auth.Get("/:id", middleware.NewAuthMMiddleware(), handler.Detail)
+	auth.Put("/:id", middleware.NewAuthMMiddleware(), handler.Update)
+	auth.Get("/generate-secret/:id", middleware.NewAuthMMiddleware(), handler.GenerateSecret)
+	auth.Delete("/:id", middleware.NewAuthMMiddleware(), handler.Delete)
 }
--- app/repositories/client_repository.go
+++ app/repositories/client_repository.go
@@ -3,6 +3,8 @@ package repositories
 import (
 	"sso-auth/app/facades"
 	"sso-auth/app/models"
+	"sso-auth/app/responses"
+	"sso-auth/app/utils"
 
 	"gorm.io/gorm"
 )
@@ -30,4 +32,36 @@ func (r *ClientRepository) CheckClientId(clientId string) error {
 	var data *models.Client
 	res := r.orm.Where("client_id = ?", clientId).First(&data)
 	return res.Error
+}
+
+func (r *ClientRepository) List(scan *[]models.Client, page, limit int, sort string) (*utils.Pagination, error) {
+	var pagination utils.Pagination
+
+	var paginate func(methods *gorm.DB) *gorm.DB
+
+	q := r.orm.Model(&models.Client{})
+
+	paginate = pagination.SetLimit(limit).SetPage(page).SetSort(sort).Pagination(q)
+
+	preQuery := facades.Orm().Scopes(paginate).Model(&models.Client{})
+
+	err := preQuery.Scan(&scan)
+
+	return &pagination, err.Error
+}
+
+func (r *ClientRepository) Detail(scan *responses.ClientDetail, clientId uint) error {
+	db := r.orm.First(&models.Client{}, clientId).Scan(scan)
+	return db.Error
+}
+
+func (r *ClientRepository) Update(data *models.Client, clientId uint) error {
+	db := r.orm.Model(&models.Client{}).Where("id", clientId).Updates(data)
+
+	return db.Error
+}
+
+func (r *ClientRepository) Delete(clientId uint) error {
+	db := r.orm.Delete(&models.Client{}, clientId)
+	return db.Error
 }
