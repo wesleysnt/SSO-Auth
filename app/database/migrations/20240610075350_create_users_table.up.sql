c86e162bb029abb587c8dec40a1090c9a8c351be
Wesley Santana <wesley.santana@redtech.co.id>
2024-07-05 10:42:04 +0700

    add migration and create make migration command

--- app/database/migrations/20240610075350_create_users_table.up.sql
+++ app/database/migrations/20240610075350_create_users_table.up.sql
@@ -2,11 +2,11 @@ CREATE TABLE users (
   id SERIAL PRIMARY KEY,
   email varchar(255) NOT NULL,
   password varchar(255) NOT NULL,
-  username varchar(50) NOT NULL,
+  name varchar(255) NOT NULL,
+  phone varchar(255) NOT NULL,
   created_at timestamp NULL,
   updated_at timestamp NULL,
   deleted_at timestamp NULL
 
 );
 create index idx_users_email on users(email);
-create index idx_users_username on users(username);
