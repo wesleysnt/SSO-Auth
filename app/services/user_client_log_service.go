68549bfabb2b6e0c3bf58b6f6563f1a65d85bc39
Wesley Santana <wesley.santana@redtech.co.id>
2024-07-07 16:49:02 +0700

    add

---
+++ app/services/user_client_log_service.go
@@ -0,0 +1,41 @@
+package services
+
+import (
+	"sso-auth/app/repositories"
+	"sso-auth/app/responses"
+	"sso-auth/app/schemas"
+	"sso-auth/app/utils"
+)
+
+type UserClientLogService struct {
+	userClientLogRepository *repositories.UserClientLogRepository
+}
+
+func NewUserClientLogService() *UserClientLogService {
+	return &UserClientLogService{
+		userClientLogRepository: repositories.NewUserClientLogRepository(),
+	}
+}
+
+func (s *UserClientLogService) List(page, limit, sort string) (*utils.Pagination, error) {
+	var scan []responses.UserClientLogResponse
+	paginateRequest := utils.PaginationRequest{
+		Page:  page,
+		Limit: limit,
+		Sort:  sort,
+	}
+	pPage, pLimit, pSort := paginateRequest.SetPagination()
+
+	res, err := s.userClientLogRepository.List(&scan, pPage, pLimit, pSort)
+
+	if err != nil {
+		return nil, &schemas.ResponseApiError{
+			Status:  schemas.ApiErrorUnprocessAble,
+			Message: err.Error(),
+		}
+	}
+
+	res.Rows = &scan
+
+	return res, nil
+}
